966ff04bbf1828667936434122d543abcfad48f7
Karl Golka <contact@karlgolka.com>
2021-09-21 16:45:24 -0700

    feat: UI: nice svg background to match color palette, better responsive, tactics for sprite figured out, divided between FE and BE

--- apps/rate-test-karlgolka-fe/app/card/Card.tsx
+++ apps/rate-test-karlgolka-fe/app/card/Card.tsx
@@ -5,6 +5,10 @@ import styles from './Card.module.scss';
 /* eslint-disable-next-line */
 export interface CardProps {}
 
+// aka `a, 2, 3, 4, 5, 6, 7, 8, 9, 10, j, q, k` *  `spade, diamonds, hearts, clubs`
+// VIP: cards that match must be replaced by a placeholder image, but keep their spots "intact!" 
+// ... otherwise not a very good memory game!
+
 export function Card(props: CardProps) {
   return (
     <div className={styles.card}>
