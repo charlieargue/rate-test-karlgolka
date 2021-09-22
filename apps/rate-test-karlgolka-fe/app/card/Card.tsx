daa67507b84cb88a7049b9bece2fbc64542a85be
Karl Golka <contact@karlgolka.com>
2021-09-21 17:06:09 -0700

    feat: UI: coming along, cards sprite working, UI looking good, ready for some mocked-functionality

--- apps/rate-test-karlgolka-fe/app/card/Card.tsx
+++ apps/rate-test-karlgolka-fe/app/card/Card.tsx
@@ -3,20 +3,27 @@ import React from 'react';
 import styles from './Card.module.scss';
 
 /* eslint-disable-next-line */
-export interface CardProps {}
+export interface CardProps { }
+
+interface SpriteMap {
+  [key: string]: number[][]
+}
 
 // aka `a, 2, 3, 4, 5, 6, 7, 8, 9, 10, j, q, k` *  `spade, diamonds, hearts, clubs`
+const cardSpriteMap: SpriteMap = {
+  "spade-a": [[0, 0], [100, 150]],
+  // "hearts-7": [[0, 0], [100, 150]],
+}
 // VIP: cards that match must be replaced by a placeholder image, but keep their spots "intact!" 
 // ... otherwise not a very good memory game!
 
 export function Card(props: CardProps) {
   return (
-    <div className={styles.card}>
-      <h1>Welcome to Card!</h1>
-      <p>one</p>
-      <p>one</p>
-      <p>one</p>
-      <p>one</p>
+    <div className={styles.card} style={{
+
+    }}>
+      <div className={styles.cardGuts}>
+      </div>
     </div>
   );
 }
