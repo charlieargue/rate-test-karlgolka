1f4c880e2e33512468eceb08180adb6d871c77c0
Karl Golka <contact@karlgolka.com>
2021-09-22 08:42:57 -0700

    feat: BUG SQUASH ATTEMPT - 1 (parentheses and then with dummy console the Promse.alls)

--- apps/rate-test-karlgolka-fe/components/game-container/GameContainer.tsx
+++ apps/rate-test-karlgolka-fe/components/game-container/GameContainer.tsx
@@ -57,19 +57,17 @@ export function GameContainer(props: GameContainerProps) {
     // check if have match, and act accordingly
     if (haveMatch()) {
       // A) got a match, make them both isMatch=true + isTurned=false and thereby hide from board
-      await Promise
-        .all([
+      await (Promise.all([
           flipCard({ gameId: data.game.id, cardId: turnedCards[0].id, isTurned: false, isMatched: true }),
           flipCard({ gameId: data.game.id, cardId: turnedCards[1].id, isTurned: false, isMatched: true })
-        ])
+        ]).then(() => console.log("after then")))
       setIsComparing(false)
     } else {
       // B) no match, so isTurned should be false for both (fire two flips!)
-      await Promise
-        .all([
+      await (Promise.all([
           flipCard({ gameId: data.game.id, cardId: turnedCards[0].id, isTurned: false, isMatched: false }),
           flipCard({ gameId: data.game.id, cardId: turnedCards[1].id, isTurned: false, isMatched: false })
-        ])
+        ]).then(() => console.log("after then")))
       setIsComparing(false)
     }
   }, [data, flipCard, haveMatch])
