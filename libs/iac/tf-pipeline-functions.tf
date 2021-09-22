322d7864f2de199598de1fa6073e563ceb750584
Karl Golka <contact@karlgolka.com>
2021-09-22 01:46:58 -0700

    feat: BE: flipCard pipeline trbl-shooting - 1

--- libs/iac/tf-pipeline-functions.tf
+++ libs/iac/tf-pipeline-functions.tf
@@ -9,7 +9,7 @@ resource "aws_appsync_function" "get_game_function" {
   data_source               = aws_appsync_datasource.ratetest_dynamodb_game_datasource.name
   name                      = "get_cart_function"
   request_mapping_template  = file("./AppSync/functions/getGame/request-mapping.vtl")
-  response_mapping_template = file("./AppSync/functions/getGame/response-mapping.vtl")
+  response_mapping_template = file("./AppSync/resolvers/_generic/generic-response-mapping-item-SINGULAR.vtl")
 }
 
 resource "aws_appsync_function" "flip_card_function" {
