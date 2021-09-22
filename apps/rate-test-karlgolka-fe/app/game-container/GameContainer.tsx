55c67563af70914876ba952438174e179495b088
Karl Golka <contact@karlgolka.com>
2021-09-21 18:28:44 -0700

    feat: UI: full page loading indicator

--- apps/rate-test-karlgolka-fe/app/game-container/GameContainer.tsx
+++ apps/rate-test-karlgolka-fe/app/game-container/GameContainer.tsx
@@ -2,24 +2,28 @@ import React from 'react';
 import Card from '../card/Card';
 import { useRouter } from 'next/router'
 import styles from './GameContainer.module.scss';
+import Loading from '../loading/loading';
 
 /* eslint-disable-next-line */
 export interface GameContainerProps { }
 
 export function GameContainer(props: GameContainerProps) {
-  
+
   // just testing
   const router = useRouter()
   console.log(router.query.game);
 
-
-  return (
-    <div className={styles.game}>
-      {new Array(24).fill({}).map((a, idx) => (
-        <Card key={idx} />
-      ))}
-    </div>
-  );
+  if (router.query.game) {
+    return (
+      <div className={styles.game}>
+        {new Array(24).fill({}).map((a, idx) => (
+          <Card key={idx} />
+        ))}
+      </div>
+    )
+  } 
+  return <Loading />
+  // return idle -> make a new game!
 }
 
 export default GameContainer;
